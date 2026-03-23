45d2b8c916c55bda218ee82c31dcc6a97c4dc36c
Kevin <4932439-kev-m@users.noreply.gitlab.com>
2026-03-23 14:32:53 +0100

    Bumping version.

--- src/measureme/__init__.py
+++ src/measureme/__init__.py
@@ -1,4 +1,4 @@
 """A vendor-abstract health data library."""
 
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "0.3.0" # Adding weight and exercise importers. Timezone fixes, weight unit and conversion fixes.
+__version__ = "0.4.0" # Adding explicit timezone support to holiday CSV. Adding holiday tests. Exposing ingest_fitout as a top-level tool, with README.
