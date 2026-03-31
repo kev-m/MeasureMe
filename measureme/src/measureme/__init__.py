de307911f8fbb46704da125b9a2b1e053112e50a
Kevin <4932439-kev-m@users.noreply.gitlab.com>
2026-03-31 10:16:27 +0200

    Bumping version.

--- measureme/src/measureme/__init__.py
+++ measureme/src/measureme/__init__.py
@@ -1,6 +1,6 @@
 """A vendor-abstract health data library."""
 
 # Semantic Versioning according to https://semver.org/spec/v2.0.0.html
-__version__ = "1.1.0" # Restructured project, to bring in FitBitMe and MeasureMe-Web as sub-projects.
+__version__ = "1.1.1" # Minor fixes
 
 from .query import MeasureMeQuery
